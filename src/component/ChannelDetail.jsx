dccbb9e769a7e8c01f6d9c27329281484a6a4ad7
Nasrullah Nadiri <nasrullahnadiri@gmail.com>
2022-09-20 19:24:07 +0500

    done with channel details page and channel cards displaying videos

--- src/component/ChannelDetail.jsx
+++ src/component/ChannelDetail.jsx
@@ -7,21 +7,38 @@ import { Videos, ChannelCard } from './';
 
 const ChannelDetail = () => {
   const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
   const { id } = useParams();
-
-  console.log(channelDetail);
-
+  console.log(videos);
   useEffect(() => {
     fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
       setChannelDetail(data?.items[0])
     );
 
-    fetchFromAPI(`search?part=snippet&id=${id}`).then((data) =>
-      setChannelDetail(data?.items[0])
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
     );
   }, [id]);
 
-  return <Box>{id}</Box>;
+  return (
+    <Box minHeight={'95vh'}>
+      <Box>
+        <div
+          style={{
+            background:
+              'linear-gradient(90deg, rgba(0,212,255,1) 0%, rgba(99,99,172,1) 48%, rgba(0,212,255,1) 100%)',
+            zIndex: '10',
+            height: '300px',
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop='-110px' />
+      </Box>
+      <Box display={'flex'} p={'2'}>
+        <Box sx={{ mr: { sm: '100px' } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
+  );
 };
 
 export default ChannelDetail;
