3d17ed078282e48ee1c6be3b11ca3d78c833b174
Nasrullah Nadiri <nasrullahnadiri@gmail.com>
2022-09-22 21:20:58 +0500

    hidden my api key from getting pushed to github

--- src/component/VideoDetail.jsx
+++ src/component/VideoDetail.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { ReactPlayer } from 'react-player';
+import { Typography, Box, Stack } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import { video } from './';
+import { fetchFromAPI } from '../utils/FetchFromAPI';
 
 const VideoDetail = () => {
-  return (
-    <div>VideoDetail</div>
-  )
-}
+  const { id } = useParams();
+  const [videoDetail, setVideoDetail] = useState(null);
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet, statistics&id=${id}`).then((data) =>
+      setVideoDetail(data.items[0])
+    );
+  }, [id]);
 
-export default VideoDetail
+  return (
+    <Box minHeight='95vh'>
+      <Stack direction={{ xs: 'column', md: 'row' }}>
+        <Box flex={1}>
+          <Box sx={{ width: '100%', position: 'sticky', top: '86px' }}>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} />
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
+
+export default VideoDetail;
